09404323aad038f5ef64c9c225e5ebd45fa62b1a
Daniel <arreoladani@yahoo.com>
2023-04-17 20:13:00 -0700

    fixed

--- js/site.js
+++ js/site.js
@@ -1,12 +1,38 @@
-function displayMessage() {
-    let msg = document.getElementById('message').value;
-    // alert(msg);
+// Controller
+function getValues() {
+    // get the user's input
+    // decide what to do with it 
+    let userInput = document.getElementById('message').value;
+    let reversedInput = reverseString(userInput);
 
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'App Name',
-            text: msg
-        }
-    );
+    displayString(reversedInput)
 }
+
+// Business logic
+function reverseString(message){
+    // take string, return it in reverse
+    let reversedMessage = '';
+    // todo: reverse the string
+    // reversedMessage = message.split('').reverse().join('');
+    for (let index = message.length - 1; index >= 0; index = index - 1){
+        reversedMessage = reversedMessage + message[index];
+    }
+
+    return reversedMessage;
+}
+
+// View
+function displayString(reverseMessage){
+    // show string on page
+    document.getElementById('msg').textContent = reverseMessage;
+    document.getElementById('alert').classList.remove('d-none');
+}
+
+
+    // Swal.fire(
+    //     {
+    //         backdrop: false,
+    //         title: 'App Name',
+    //         text: msg
+    //     }
+    // );
